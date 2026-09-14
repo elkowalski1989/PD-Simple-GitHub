5ce2f5bc165c41522c32bec4e92e001a000c58f2
elkowalski1989 <elkowals@us.ibm.com>
2026-09-14 18:27:06 -0500

    Allow Board Explorer cold native attach

--- samples/BoardExplorer/EngineFirstWindow.xaml.cs
+++ samples/BoardExplorer/EngineFirstWindow.xaml.cs
@@ -27,6 +27,9 @@ public partial class EngineFirstWindow : Window
         _session = AllegroEngineSession.Create(
             new EngineSessionOptions
             {
+                // A cold native attach can outlive Engine's short generic default,
+                // especially while Allegro is completing resident startup.
+                ConnectionTimeout = TimeSpan.FromSeconds(30),
                 RequiredCapabilities =
                 [
                     EngineCapabilities.SceneRead,
